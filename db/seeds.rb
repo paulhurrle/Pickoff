15d73aee3ddfe250a5839c47651304c4594104d8
paulhurrle <paul_hurrle@yahoo.com>
2017-11-21 21:29:15 -0700

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,32 +40,6 @@ end
     
     item.update_attribute(:created_at, Faker::Time.between(1.year.ago, Time.now, :all))
 end
-#
-#require 'faker'
-#
-#Faker::Name.unique.name       #=> "Christophe Bartell"
-#Faker::Internet.email         #=> "kirsten.greenholt@corkeryfisher.info"
-#
-## Create Users
-#15.times do
-#	User.create!(
-#		email: Faker::Internet.email,
-#		password: 'password',
-#	)
-#end
-#users = User.all
-#
-## Create Wikis
-#50.times do
-#	wiki = Wiki.create!(
-#		title: Faker::Hacker.say_something_smart,
-#		body:  Faker::Lorem.paragraph,
-#		user:  users.sample,
-#	)	
-#
-#	wiki.update_attribute(:created_at, Faker::Time.between(1.year.ago, Time.now, :all))
-#end
-#wikis = Wiki.all
 
 puts "Seed finished"
 puts "#{User.count} users created"
