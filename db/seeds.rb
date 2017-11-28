bddf9b4990bf300836489cdfebb999b733030c88
paulhurrle <paul_hurrle@yahoo.com>
2017-11-27 17:35:26 -0700

    conserve memory by storing test emails inside an array and by storing User.all inside a global variable

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,71 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
 
 # Create users
-User.create!(
-    email: 'paul_hurrle@yahoo.com',
-    password: 'password',
-    )
 
-User.create!(
-    email: 'paul_nikki@cox.net',
+test_users = ['paul_hurrle@yahoo.com', 'paul_nikki@cox.net', 'paulnicolahurrle@gmail.com']
+test_users.each do |user|
+	User.create!(
+	first_name: 'Paul',
+	last_name: 'Hurrle',
+    email: user,
     password: 'password',
     )
-
-User.create!(
-    email: 'paulnicolahurrle@gmail.com',
-    password: 'password',
-    )
+end
 
 10.times do
    User.create!(
-#       name: Faker::Name.name,
+	   first_name: Faker::Name.first_name,
+	   last_name: Faker::Name.last_name,
        email: Faker::Internet.email,
        password: 'password',
        ) 
 end
 
+users = User.all #grabs all users for sampling below
+
 # Create items
+
 25.times do
    item = Item.create!(
-       user_id: User.all.sample.id,
+       user_id: users.sample.id,
        name: Faker::Movie.quote,
        )
     
     item.update_attribute(:created_at, Faker::Time.between(1.year.ago, Time.now, :all))
 end
-#
-#require 'faker'
-#
-#Faker::Name.unique.name       #=> "Christophe Bartell"
-#Faker::Internet.email         #=> "kirsten.greenholt@corkeryfisher.info"
-#
-## Create Users
-#15.times do
-#	User.create!(
-#		email: Faker::Internet.email,
-#		password: 'password',
-#	)
-#end
-#users = User.all
-#
-## Create Wikis
-#50.times do
-#	wiki = Wiki.create!(
-#		title: Faker::Hacker.say_something_smart,
-#		body:  Faker::Lorem.paragraph,
-#		user:  users.sample,
-#	)	
-#
-#	wiki.update_attribute(:created_at, Faker::Time.between(1.year.ago, Time.now, :all))
-#end
-#wikis = Wiki.all
 
 puts "Seed finished"
 puts "#{User.count} users created"
